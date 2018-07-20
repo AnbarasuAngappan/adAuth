f3f88ef297b632837430ec105c1597c3c829a6cb
Anbarasu Angappan <anburvhs@hotmail.com>
2018-07-20 13:27:47 +0530

    Add project files.

---
+++ adAuth/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace adAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {           
+            if (((System.Security.Claims.ClaimsIdentity)User.Identity).HasClaim(ClaimTypes.Name, "Anbarasu"))
+            {
+                ViewBag.Message = "Your application description page.";
+                return View();
+            }
+            return View();           
+        }
+
+        [Authorize(Users = "Anbarasu")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
---
+++ adAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(adAuth.Startup))]
+namespace adAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
